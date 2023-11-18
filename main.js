7e0215fca82ff77753dd64fb824a3ffd0b8ceb1d
ryuuya <ryuuya0921@gmail.com>
2023-11-19 04:42:23 +0900

    firstcommit

---
+++ main.js
@@ -0,0 +1,63 @@
+// STARTボタンのDOMを取得
+
+const timeArea = document.getElementById('time')//表示する時間
+
+const startButton = document.getElementById('startButton');//開始する
+console.log(startButton);
+
+const stopButton = document.getElementById('stopButton');//停止する
+console.log(stopButton);
+
+const resetButton = document.getElementById('resetButton');//リセットする
+console.log(resetButton);
+
+//開始時間
+
+let startTime;
+
+//停止時間
+
+let stopTime = 0;
+
+// タイムアウトID
+let timeoutID;
+
+//スタートボタンの設定
+
+function displayTime() {
+  const currentTime = new Date(Date.now() - startTime + stopTime);
+  const h = String(currentTime.getHours()-1).padStart(2, '0');
+  const m = String(currentTime.getMinutes()).padStart(2, '0');
+  const s = String(currentTime.getSeconds()).padStart(2, '0');
+  const ms = String(currentTime.getMilliseconds()).padStart(3, '0');
+
+  time.textContent = `${h}:${m}:${s}.${ms}`;
+  timeoutID = setTimeout(displayTime, 1);
+}
+
+// スタート設定
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  resetButton.disabled = true;
+  startTime = Date.now();
+  displayTime();
+});
+
+//ストップ
+stopButton.addEventListener('click', function() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = false;
+  clearTimeout(timeoutID);
+  stopTime += (Date.now() - startTime);
+});
+
+//リセット
+resetButton.addEventListener('click', function() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  resetButton.disabled = true;
+  time.textContent = '00:00:00.000';
+  stopTime = 0;
+});
